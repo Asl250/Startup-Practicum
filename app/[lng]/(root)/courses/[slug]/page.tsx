bbbae834faace880fecb01279eb5001c0d07cf13
ask250 <aslrahidov250@gmail.com>
2024-08-15 18:54:07 +0500

    Detailed course. UI

--- app/[lng]/(root)/courses/[slug]/page.tsx
+++ app/[lng]/(root)/courses/[slug]/page.tsx
@@ -1,8 +1,55 @@
 'use client'
 
+import Description from '@/app/[lng]/(root)/courses/[slug]/_components/Description'
+import Hero from '@/app/[lng]/(root)/courses/[slug]/_components/Hero'
+import Overview from '@/app/[lng]/(root)/courses/[slug]/_components/Overview'
+import CourseCard from '@/components/cards/course.card'
+import TopBar from '@/components/shared/top-bar'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
+import { Separator } from '@/components/ui/separator'
+import { courses } from '@/constants'
+import useTranslate from '@/hooks/use-translate'
+import type { ICourse } from '@/types'
+
 const Page = () => {
+	const t = useTranslate()
+	
 	return (
-		<div>Page</div>
+		<>
+			<TopBar label={'allCourses'} extra={'Full Courses ReactJs'}/>
+			
+			<div className={'container mx-auto max-w-6xl'}>
+				<div className={'grid grid-cols-3 gap-4 pt-12'}>
+					<div className={'col-span-2 max-lg:col-span-3'}>
+						<Hero />
+						<Overview/>
+					</div>
+					<div className={'col-span-1 max-lg:col-span-3'}>
+						<Description/>
+					</div>
+				</div>
+				
+				<Separator className={'my-12'}/>
+				<h1 className='font-space-grotesk text-4xl font-bold'>
+					{t('youMayLike')}
+				</h1>
+				<Carousel opts={{ align: 'start' }} className='mt-6 w-full'>
+					<CarouselContent className='w-full'>
+						{courses.map((course: ICourse) => (
+							<CarouselItem
+								key={course.title}
+								className='md:basis-1/2 lg:basis-1/3'
+							>
+								<CourseCard {...course} />
+							</CarouselItem>
+						))}
+					</CarouselContent>
+					<CarouselPrevious className={'flex max-lg:hidden'}/>
+					<CarouselNext className={'flex max-lg:hidden'}/>
+				</Carousel>
+				
+			</div>
+		</>
 	)
 }
 export default Page
---
+++ components/cards/review.card.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import ReactStars from 'react-stars'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { useParams } from 'next/navigation'
+import useTranslate from '@/hooks/use-translate'
+
+function ReviewCard() {
+	const { lng } = useParams()
+	const t = useTranslate()
+
+	return (
+		<div className='mt-6 border-t border-t-secondary'>
+			<div className='mt-8 flex gap-2'>
+				<Avatar>
+					<AvatarImage src={'https://blog.sammi.app/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FKmuGvX9oRjYkG3bzttvc&w=1920&q=75'} />
+					<AvatarFallback className='uppercase'>
+						Chris Impley
+					</AvatarFallback>
+				</Avatar>
+
+				<div className='flex flex-col'>
+					<div>Chris Impley</div>
+					<div className='flex items-center gap-1'>
+						<ReactStars value={4.2} edit={false} color2='#DD6B20' />
+						<p className='text-sm opacity-50'>
+							5 minut oldin{' '}
+							{t('ago')}
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className='mt-2'>kurs bomba</div>
+		</div>
+	)
+}
+
+export default ReviewCard
