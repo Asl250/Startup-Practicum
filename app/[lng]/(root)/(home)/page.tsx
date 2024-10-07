a3f06ca6bc3bc44264be7c4ff6b0f91b22504808
ASK250 <137773908+Asl250@users.noreply.github.com>
2024-10-07 14:56:24 +0500

    Update bg

--- app/[lng]/(root)/(home)/page.tsx
+++ app/[lng]/(root)/(home)/page.tsx
@@ -13,7 +13,7 @@ const Home = async () => {
 	return (
 		<>
 				<div
-					className="bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]">
+					className="">
 					<Hero />
 					{courses.length > 3 && <FeaturedCourses courses={JSON.parse(JSON.stringify(courses))} />}
 					<Categories />
