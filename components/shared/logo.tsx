5789186bfb5c581fbfbe9022eb7e551781472335
ask250 <aslrahidov250@gmail.com>
2024-12-12 21:08:34 +0500

    update

--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
 	return (
 		<Link href={'/'} className={'flex items-center gap-2'}>
 			<Image src={"/logo.png"} alt={'logo'} width={70} height={70}/>
-			<h1 className={'font-spaceGrotesk text-4xl font-bold'}>Ta'limot</h1>
+			<h1 className={'font-spaceGrotesk text-4xl font-bold'}>Ta&apos;limot</h1>
 		</Link>
 	)
 }
